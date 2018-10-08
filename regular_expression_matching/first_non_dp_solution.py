48a8aac0a3a7236ba9b1f4de59bc1e822c15e371
Beomsoo Jang <beomsoo.jang@lge.com>
2018-10-08 22:07:22 +0900

    removed test cases from solution files of regular_expression_matching (#8)

--- regular_expression_matching/first_non_dp_solution.py
+++ regular_expression_matching/first_non_dp_solution.py
@@ -1,5 +1,3 @@
-import unittest
-
 class Solution:
     def isMatch(self, s, p):
         """
@@ -17,52 +15,3 @@ class Solution:
                     first_match and self.isMatch(s[1:], p))
         else:
             return first_match and self.isMatch(s[1:], p[1:])
-
-class TestSolution(unittest.TestCase):
-    def test_all_none(self):
-        s = ""
-        p = ""
-        self.assertTrue(Solution().isMatch(s, p))
-
-    def test_none_string(self):
-        s = ""
-        p = "a"
-        self.assertFalse(Solution().isMatch(s, p))
-
-    def test_no_symbol_equal(self):
-        s = "abcd"
-        p = "abcd"
-        self.assertTrue(Solution().isMatch(s, p))
-
-    def test_no_symbol_not_equal_with_equal_length(self):
-        s = "abcd"
-        p = "defg"
-        self.assertFalse(Solution().isMatch(s, p))
-
-    def test_no_symbol_not_equal_with_not_equal_length(self):
-        s = "abcd"
-        p = "abcde"
-        self.assertFalse(Solution().isMatch(s, p))
-
-    def test_no_test_symbol_pattern(self):
-        s = ""
-        p = "ab*"
-        self.assertFalse(Solution().isMatch(s, p))
-
-    def test_matched_symbol_pattern_1(self):
-        s = "ab"
-        p = "ab*"
-        self.assertTrue(Solution().isMatch(s, p))
-
-    def test_matched_symbol_pattern_2(self):
-        s = "a"
-        p = "ab*"
-        self.assertTrue(Solution().isMatch(s, p))
-
-    def test_not_matched_duplicated_pattern(self):
-        s = "aaaaaaaaab"
-        p = "a*a*a*a*a*a*a*a*a*a*c"
-        self.assertFalse(Solution().isMatch(s, p))
-
-if __name__ == "__main__":
-    unittest.main()
