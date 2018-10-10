3a91e76377f9a5987d5979eb8ff87a7917c7a24f
Beomsoo Jang <beomsoo.jang@lge.com>
2018-10-10 14:32:18 +0900

    updated python solutions for #6 (Reverse Integer)

--- leetcode/reverse_integer/solution.py
+++ leetcode/reverse_integer/solution.py
@@ -5,18 +5,16 @@ class Solution:
         :rtype: int
         """
         INT_MAX = 2147483647;
-        INT_MIN = -2147483648;
+        INT_MIN = 2147483648;  # actually it is -2147483648
+                               # but change it because in python,
+                               # modulo works differently with c's modulo
 
-        if x>INT_MAX or x<INT_MIN: return 0
-
-        m = x if x>0 else -1*x
+        m, LIMIT = (x, INT_MAX) if x > 0 else (-1*x, INT_MIN)
         rev = 0
 
-        while m>0:
+        while m!=0:
+            if rev > LIMIT//10 or (rev == LIMIT//10 and m%10 > LIMIT%10): return 0
             rev = rev*10 + m%10
             m = m // 10
 
-        if x<0: rev = -1*rev
-
-        return 0 if rev>INT_MAX or rev<INT_MIN else rev
-        
+        return rev if x > 0 else -1*rev
